23c45a582fbe2d3dda06bd80b7af2669b9392621
Leo Tu <ll410119@gmail.com>
2018-01-07 01:25:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,23 +6,24 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element);
+  var newArray = array;
+  newArray.unshift(element);
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var newArray = array;
   newArray.unshift(element);
   return newArray;
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  var newArray = array;
+  newArray.push(element);
+  return newArray;
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  var newArray = array;
+  
   newArray.push(element);
   return newArray;
 }
