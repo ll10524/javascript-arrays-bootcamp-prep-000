605947c724be8d9e013f43f7149118f0f81b4dd5
Leo Tu <ll410119@gmail.com>
2018-01-07 01:13:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var newArray = array;
+  newArray.unshift(element);
+  return Array;
+}
